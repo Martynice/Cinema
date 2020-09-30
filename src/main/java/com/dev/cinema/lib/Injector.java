831ef4cd31ab6389de4706a1170aaa28f38eabe2
Martynice <marticiy@ukr.net>
2020-09-30 17:31:16 +0300

    added empty line

--- src/main/java/com/dev/cinema/lib/Injector.java
+++ src/main/java/com/dev/cinema/lib/Injector.java
@@ -120,7 +120,6 @@ public class Injector {
      * @throws ClassNotFoundException if the class cannot be located
      * @throws IOException            if I/O errors occur
      */
-
     private static List<Class<?>> getClasses(String packageName)
             throws IOException, ClassNotFoundException {
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
@@ -149,7 +148,6 @@ public class Injector {
      * @return The classes
      * @throws ClassNotFoundException if the class cannot be located
      */
-
     private static List<Class<?>> findClasses(File directory, String packageName)
             throws ClassNotFoundException {
         List<Class<?>> classes = new ArrayList<>();
