7620ce44b653c20149d05587ec8e29ee29c6cbad
Martynice <marticiy@ukr.net>
2020-10-07 18:29:55 +0300

    added user entity, authentication service and implementation

---
+++ src/main/java/com/dev/cinema/security/AuthenticationServiceImpl.java
@@ -0,0 +1,38 @@
+package com.dev.cinema.security;
+
+import com.dev.cinema.exceptions.AuthenticationException;
+import com.dev.cinema.lib.Inject;
+import com.dev.cinema.lib.Service;
+import com.dev.cinema.model.User;
+import com.dev.cinema.service.UserService;
+import com.dev.cinema.util.HashUtil;
+import java.util.Optional;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> userFromDB = userService.findByEmail(email);
+        if (userFromDB.isEmpty() || !isValid(password, userFromDB.get())) {
+            throw new AuthenticationException("Incorrect login or password");
+        }
+        return userFromDB.get();
+    }
+
+    @Override
+    public User register(String email, String password) {
+        User user = new User();
+        byte[] salt = HashUtil.getSalt();
+        user.setEmail(email);
+        user.setPassword(HashUtil.hashPassword(password, salt));
+        user.setSalt(salt);
+        return userService.add(user);
+    }
+
+    private boolean isValid(String password, User user) {
+        return HashUtil.hashPassword(password, user.getSalt()).equals(user.getPassword());
+    }
+}
